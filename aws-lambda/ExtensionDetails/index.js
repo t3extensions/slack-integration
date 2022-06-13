ceaa98929816a94070381c9dd79f915c96713cae
Michael Schams <noreply@example.com>
2022-06-13 11:33:03 +1000

    [TASK] Bump version to v2.02 and update copyright year

--- aws-lambda/ExtensionDetails/index.js
+++ aws-lambda/ExtensionDetails/index.js
@@ -1,9 +1,9 @@
 /**
  * ExtensionDetails
  *
- * (c)2020-2021 by Michael Schams <schams.net>
+ * (c)2020-2022 by Michael Schams <schams.net>
  * https://t3extensions.org
- * Version 2.01
+ * Version 2.02
  */
 
 'use strict';
